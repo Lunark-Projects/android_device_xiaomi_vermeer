503b2c791d88dc611362c1a05e5d334c0185e52f
Spacemk1014 <jvillaltamatos3007@gmail.com>
2024-09-12 19:47:14 -0500

    vermeer: Adapt dt for Infinity X

--- infinity_vermeer.mk
+++ infinity_vermeer.mk
@@ -8,14 +8,22 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common infinity X stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Inherit from vermeer device
 $(call inherit-product, device/xiaomi/vermeer/device.mk)
 
+# Infinity X Settings
+INFINITY_BUILD_TYPE := UNOFFICIAL
+INFINITY_MAINTAINER := Justin117(Spacemk)
+TARGET_SUPPORTS_BLUR := true
+TARGET_HAS_UDFPS := true
+WITH_GAPPS := true
+TARGET_BUILD_GOOGLE_TELEPHONY := true
+
 PRODUCT_DEVICE := vermeer
-PRODUCT_NAME := lineage_vermeer
+PRODUCT_NAME := infinity_vermeer
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := 23113RKC6G
 PRODUCT_MANUFACTURER := xiaomi
