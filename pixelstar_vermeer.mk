3b1de63a1b6d0efd373f12b1dcf2d31abe0c675e
Spacemk1014 <jvillaltamatos3007@gmail.com>
2024-11-11 21:43:51 -0500

    vemeer: Bringup! PixelStar

--- pixelstar_vermeer.mk
+++ pixelstar_vermeer.mk
@@ -8,14 +8,27 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelStar stuff.
+$(call inherit-product, vendor/pixelstar/config/common_full_phone.mk)
 
 # Inherit from vermeer device
 $(call inherit-product, device/xiaomi/vermeer/device.mk)
 
+# PixelStar flags
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+PIXELSTAR_BUILD_TYPE := official
+USE_PIXEL_CHARGER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+PRODUCT_NO_CAMERA := true
+ADD_CUSTOM_APPS := true
+
 PRODUCT_DEVICE := vermeer
-PRODUCT_NAME := lineage_vermeer
+PRODUCT_NAME := pixelstar_vermeer
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := 23113RKC6G
 PRODUCT_MANUFACTURER := xiaomi
