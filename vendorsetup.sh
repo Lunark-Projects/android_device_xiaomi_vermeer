8edd99d4b97cb86edfb26de752a8a9e0e035d9e2
Spacemk1014 <jvillaltamatos3007@gmail.com>
2024-11-11 21:53:59 -0500

    vermeer: Use my repo for Poco F6 Pro / Redmi K70

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,29 +1,26 @@
 # Vendor  
-git clone --depth=1 https://github.com/Spacemk1014/proprietary_vendor_xiaomi.git vendor/xiaomi 
+git clone --depth=1 https://github.com/Lunark-Projects/vendor_xiaomi.git vendor/xiaomi 
 
 # Kernel
 rm -rf device/xiaomi/vermeer-kernel 
-git clone --depth=1 https://github.com/Spacemk1014/android_device_xiaomi_vermeer-kernel.git device/xiaomi/vermeer-kernel 
+git clone --depth=1 https://github.com/Lunark-Projects/device_xiaomi_vermeer-kernel.git device/xiaomi/vermeer-kernel 
 
 # Camera 
-git clone https://gitlab.com/ThankYouMario/proprietary_vendor_xiaomi_camera.git -b uvite-sm8550 vendor/xiaomi/camera 
+git clone https://github.com/Lunark-Projects/proprietary_vendor_xiaomi_camera.git vendor/xiaomi/camera 
 
 # Common Tree 
 rm -rf device/xiaomi/sm8550-common 
-git clone --depth=1 https://github.com/Spacemk1014/android_device_xiaomi_sm8550-common.git device/xiaomi/sm8550-common 
+git clone --depth=1 https://github.com/Lunark-Projects/android_device_xiaomi_sm8550-common.git -b InfinityX device/xiaomi/sm8550-common 
 
 # Hardware 
-git clone --depth=1 https://github.com/Spacemk1014/android_hardware_xiaomi.git hardware/xiaomi 
+git clone --depth=1 https://github.com/Lunark-Projects/hardware_xiaomi.git hardware/xiaomi 
 
 # Sepolicy 
-git clone --depth=1 https://github.com/Spacemk1014/android_device_xiaomi_sepolicy.git device/xiaomi/sepolicy 
+git clone --depth=1 https://github.com/Lunark-Projects/android_device_xiaomi_sepolicy.git device/xiaomi/sepolicy 
 
 # Hardware Qcom
-git clone --depth=1 https://github.com/Spacemk1014/android_hardware_qcom-caf_sm8550_gps.git hardware/qcom-caf/sm8550/gps
+git clone --depth=1 https://github.com/Lunark-Projects/android_hardware_qcom-caf_sm8550_gps.git hardware/qcom-caf/sm8550/gps
 
 # Sepolicy_vndr
 rm -rf device/qcom/sepolicy_vndr/sm8550
-git clone --depth=1 https://github.com/Spacemk1014/android_device_qcom_sepolicy_vndr.git device/qcom/sepolicy_vndr/sm8550
-
-# System Qcom
-git clone --depth=1 https://github.com/Spacemk1014/android_system_qcom.git system/qcom
+git clone --depth=1 https://github.com/Lunark-Projects/device_qcom_sepolicy_vndr.git device/qcom/sepolicy_vndr/sm8550
