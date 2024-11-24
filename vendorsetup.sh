0b7ca90f34c35c4fbddc0bd6444d45ff2d40441e
Spacemk1014 <jvillaltamatos3007@gmail.com>
2024-11-23 22:19:31 -0500

    vermeer: add branch 15 in vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,7 @@
 # Vendor  
 git clone --depth=1 https://github.com/Lunark-Projects/vendor_xiaomi.git vendor/xiaomi 
 
-# Kernel
-rm -rf device/xiaomi/vermeer-kernel 
+# Kernel 
 git clone --depth=1 https://github.com/Lunark-Projects/device_xiaomi_vermeer-kernel.git device/xiaomi/vermeer-kernel 
 
 # Camera 
@@ -13,10 +12,12 @@ rm -rf device/xiaomi/sm8550-common
 git clone --depth=1 https://github.com/Lunark-Projects/android_device_xiaomi_sm8550-common.git -b InfinityX device/xiaomi/sm8550-common 
 
 # Hardware 
-git clone --depth=1 https://github.com/Lunark-Projects/hardware_xiaomi.git hardware/xiaomi 
+rm -rf hardware/xiaomi
+git clone --depth=1 https://github.com/Lunark-Projects/hardware_xiaomi.git -b 15 hardware/xiaomi 
 
 # Sepolicy 
-git clone --depth=1 https://github.com/Lunark-Projects/android_device_xiaomi_sepolicy.git device/xiaomi/sepolicy 
+rm -rf device/xiaomi/sepolicy
+git clone --depth=1 https://github.com/Lunark-Projects/android_device_xiaomi_sepolicy.git -b 15 device/xiaomi/sepolicy 
 
 # Hardware Qcom
 git clone --depth=1 https://github.com/Lunark-Projects/android_hardware_qcom-caf_sm8550_gps.git hardware/qcom-caf/sm8550/gps
